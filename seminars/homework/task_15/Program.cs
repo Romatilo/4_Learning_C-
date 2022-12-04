c39406cf901d7349d0f85bb3d2bff31f3674a2d9
tilo <romatilo.gmail.com>
2022-12-04 06:05:56 +0300

    task_15 added

---
+++ seminars/homework/task_15/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да; 7 -> да; 1 -> нет
+
+Console.Write("Enter weekday number (in the range from 1 to 7) -> ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+// Проверка, чтобы введенное число удовлетворяло условию:
+while (number < 1 || number > 7)
+{
+    Console.WriteLine("Week consists of 7 days!");
+    Console.Write("Enter day of the week number(in the range from 1 to 7) -> ");
+    number = Convert.ToInt32(Console.ReadLine());
+}
+
+// Если введенное число 6 или 7 - выходной день:
+if (number >= 6)
+{
+    Console.WriteLine("This is weekend!");
+}
+else Console.WriteLine("This is weekday.");
