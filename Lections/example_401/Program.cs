d693481c2c0f0f76cac9f8b006d2cfa2dae76a1f
tilo <romatilo.gmail.com>
2022-12-17 13:26:55 +0300

    example 401 added

---
+++ Lections/example_401/Program.cs
@@ -0,0 +1,11 @@
+﻿int[,] matrix = new string[3, 4];
+
+void PrintArray (int[,] matr)
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        Console.Write($"-{matrix[i, j]}- ");
+    }
+    Console.WriteLine();
+}
