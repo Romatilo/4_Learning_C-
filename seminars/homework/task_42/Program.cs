a8b5c30b5200c1761f53416b9b47fea9f29c49af
tilo <romatilo.gmail.com>
2022-12-18 10:51:29 +0300

    HW added

---
+++ seminars/homework/task_42/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 42: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 
+// задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+// Для начала выведем значения x и y из системы уравнений:
+// |y = k1 * x + b1  | x(k1 - k2) + (b1 - b2) = 0 | x = (b2 - b1) / (k1 - k2)
+// |y = k2 * x + b2  | y = k2 * x + b2            | y =  k2(b1 - b2) / (k1 - k2) + b2
+
+Console.WriteLine("Введите значения b1 -> ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значения k1 -> ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значения b2 -> ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значения k2 -> ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+double x = Math.Round((b2 - b1) / (k1 - k2),2);
+double y = Math.Round(((k2*(b2 - b1)) / (k1 - k2) + b2),2);
+Console.WriteLine($"Прямые пересекаются в следующих координатах: ({x}; {y})");
---
+++ seminars/homework/task_43/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 43: Напишите программу, которая будет преобразовывать 
+// десятичное число в двоичное.
+// 45 -> 101101; 3 -> 11 2 -> 10
+
+/*string StringReverse(string inputString)
+{
+    char[] arr = inputString.ToCharArray();
+    int size = arr.Length;
+    char temp = arr[0];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = arr[size-i-1];
+    }
+    arr[size-1] = temp;
+    return new string(arr);
+}
+ 
+string Dec2Bin(int decimalNumber)
+{
+    string binar = "";
+    while ((decimalNumber) > 0)
+    {
+        binar = binar + decimalNumber % 2;
+        decimalNumber = decimalNumber / 2;
+    }
+    return StringReverse(binar);
+}
+
+Console.WriteLine("Введите десятичное число");
+string s = Console.ReadLine();
+Console.WriteLine(s);
+int deci = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Dec2Bin(deci));
+*/
+
+string s = Console.ReadLine();
+Console.WriteLine(s);
