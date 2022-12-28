0630202db80307c998ba3604ce42ef7a55903934
tilo <romatilo.gmail.com>
2022-12-28 18:28:08 +0300

    Homework 64, 66, 68 tasks added

---
+++ seminars/homework/task_64_Sum_natural_recursion/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт 
+// сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Write("Введите значение M --> ");
+int m = Convert.ToInt32(Console.ReadLine()); // M (левая граница)
+Console.Write("Введите значение N --> ");
+int n = Convert.ToInt32(Console.ReadLine()); // N (правая граница)
+int sum = 0;
+//  start = M, end = N
+int SumNumbers(int start, int end)
+{
+    sum += start;
+    // Базовый случай - выход из рекурсии:
+    if (start >= end) return sum;
+
+    // Рекурсивный случай:
+    return (SumNumbers(start + 1, end));
+}
+
+Console.WriteLine("Сумма натуральных чисел от M до N: " + SumNumbers(m, n));
---
+++ seminars/homework/task_68_Ackermann_function/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии.
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+// Упрощенная запись функции Аккермана (вариант Аккермена-Петера):
+// A (0,n) = n + 1
+// A (m+1, 0) = A(m,1)
+// A (m+1, n+1) = A(m, A(m+1,n))
+
+
+double A(double m, double n)
+{
+    if (m == 0) return n+1;
+    else if (n == 0) return A(m - 1, 1);
+    else return A(m - 1, A(m, n - 1));
+}
+
+Console.Write("Введите значение m --> ");
+double m = Convert.ToInt64(Console.ReadLine());
+Console.Write("Введите значение n --> ");
+double n = Convert.ToInt64(Console.ReadLine());
+Console.Write($"Значение функции Аккермана А({m},{n}) = ");
+Console.WriteLine(A(m, n));
